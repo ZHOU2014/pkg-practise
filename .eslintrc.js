b499509eac868e297a4f3281053251fe3c802347
zhoulongqiu <zhoulq@authine.com>
2022-05-23 15:43:47 +0800

    fix: change

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,15 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: ['plugin:prettier/recommended'],
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
+  rules: {},
 };
